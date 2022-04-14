4c2301fe2bae2454a1e2f8159372f06572338d70
Ezequiel <ezequielamin@outlook.com>
2022-04-14 20:30:09 -0300

    agrego carga y lectura de gastos e ingresos

--- routes/expenses/expenses.js
+++ routes/expenses/expenses.js
@@ -8,6 +8,8 @@ const generarCodigo = require("../../helpers/generarCodigo");
 const stringify = require("../../helpers/stringify");
 
 const DbExpenses = require("../../models/expense");
+const DBCategories = require("../../models/category");
+const DbAccounts = require("../../models/account");
 const editList = require("../../helpers/db/editList");
 
 router.get("/api/expenses", isAuthenticated, async (req, res) => {
@@ -15,6 +17,60 @@ router.get("/api/expenses", isAuthenticated, async (req, res) => {
 
   const document = await DbExpenses.findOne({ dni });
   res.json(document);
+});
+
+router.get("/api/expenses/listTransform", isAuthenticated, async (req, res) => {
+  const dni = process.env.NODE_ENV === "test" ? "12345678" : req.user.dni;
+
+  let document = await DbExpenses.findOne({ dni });
+  const categories = await DBCategories.findOne({ dni });
+  const accounts = await DbAccounts.findOne({ dni });
+
+  document.expenses.sort((a, b) => {
+    return b.date - a.date;
+  });
+
+  const expenses = document.expenses.map((expense) => {
+    const category = categories.categories.find(
+      (category) => category.id === expense.categoryId
+    );
+
+    const account = accounts.accounts.find(
+      (account) => account.id === expense.accountId
+    );
+
+    const date = new Date(expense.date + " UTC");
+
+    let days = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    if (days < 10) {
+      days = "0" + days;
+    }
+    if (month < 10) {
+      month = "0" + month;
+    }
+
+    const formatDate = `${days}/${month}/${year}`;
+
+    let time = date.toISOString().substr(11, 8);
+
+    return {
+      id: expense.id,
+      title: expense.title,
+      price: expense.price,
+      date: formatDate,
+      time: time,
+      category: category.title,
+      icon: category.icon,
+      color: category.color,
+      account: account.title,
+      accountColor: account.color,
+      description: expense.description,
+    };
+  });
+  return res.json(expenses);
 });
 
 router.put("/api/expense", isAuthenticated, async (req, res) => {
@@ -71,30 +127,29 @@ router.put("/api/expense/:id", isAuthenticated, async (req, res) => {
   editList("expense", dni, req.params.id, req.body, res);
 });
 
-
 router.delete("/api/expense/:id", isAuthenticated, (req, res) => {
-    const dni = process.env.NODE_ENV === "test" ? "12345678" : req.user.dni;
-  
-    DbExpenses.findOneAndUpdate(
-      { dni },
-      {
-        $pull: {
-          expenses: {
-            id: req.params.id,
-          },
+  const dni = process.env.NODE_ENV === "test" ? "12345678" : req.user.dni;
+
+  DbExpenses.findOneAndUpdate(
+    { dni },
+    {
+      $pull: {
+        expenses: {
+          id: req.params.id,
         },
       },
-      { new: true },
-      (err, expenses) => {
-        if (err) {
-          return res.status(401).json({
-            err,
-          });
-        }
-  
-        res.status(200).json(expenses);
+    },
+    { new: true },
+    (err, expenses) => {
+      if (err) {
+        return res.status(401).json({
+          err,
+        });
       }
-    );
-  });
+
+      res.status(200).json(expenses);
+    }
+  );
+});
 
 module.exports = router;
--- routes/incomes/incomes.js
+++ routes/incomes/incomes.js
@@ -8,6 +8,7 @@ const generarCodigo = require("../../helpers/generarCodigo");
 const stringify = require("../../helpers/stringify");
 
 const DbIncomes = require("../../models/income");
+const DbAccounts = require("../../models/account");
 const editList = require("../../helpers/db/editList");
 
 router.get("/api/incomes", isAuthenticated, async (req, res) => {
@@ -55,6 +56,52 @@ router.put("/api/income", isAuthenticated, async (req, res) => {
 
     res.status(200).json(document);
   });
+});
+
+router.get("/api/incomes/listTransform", isAuthenticated, async (req, res) => {
+  const dni = process.env.NODE_ENV === "test" ? "12345678" : req.user.dni;
+
+  let document = await DbIncomes.findOne({ dni });
+  const accounts = await DbAccounts.findOne({ dni });
+
+  document.incomes.sort((a, b) => {
+    return b.date - a.date;
+  });
+
+  const incomes = document.incomes.map((income) => {
+    const account = accounts.accounts.find(
+      (account) => account.id === income.accountId
+    );
+
+    const date = new Date(income.date + " UTC");
+
+    let days = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    if (days < 10) {
+      days = "0" + days;
+    }
+    if (month < 10) {
+      month = "0" + month;
+    }
+
+    const formatDate = `${days}/${month}/${year}`;
+
+    let time = date.toISOString().substr(11, 8);
+
+    return {
+      id: income.id,
+      title: income.title,
+      price: income.price,
+      date: formatDate,
+      time: time,
+      account: account.title,
+      accountColor: account.color,
+      description: income.description,
+    };
+  });
+  return res.json(incomes);
 });
 
 router.put("/api/income/:id", isAuthenticated, async (req, res) => {
