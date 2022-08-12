0bc736573b34f1325733701c85644e696db7a89d
Ezequiel Amin <ezequielamin@outlook.com>
2022-08-12 07:35:38 -0300

    hotfix for locale dates, regarding the week chart

--- routes/charts/charts.js
+++ routes/charts/charts.js
@@ -9,6 +9,7 @@ const DbCategories = require("../../models/category");
 const DbAccounts = require("../../models/account");
 const roundToNearestHour = require("../../helpers/roundToNearestHour");
 const getWeekDay = require("../../helpers/getWeekDay");
+const differenceInWeeks = require("../../helpers/differenceInWeeks");
 
 router.get("/api/charts/chartData", isAuthenticated, async (req, res) => {
   const dni = process.env.NODE_ENV === "test" ? "12345678" : req.user.dni;
@@ -70,7 +71,7 @@ router.get("/api/charts/weekChart", isAuthenticated, async (req, res) => {
       // arithmetic progression that starts in 7
       let hour = 7 + 2 * i;
       if (hour > 23) hour = hour % 24;
-      hour = hour + "hs"
+      hour = hour + "hs";
 
       const weekday = getWeekDay(day);
 
@@ -84,17 +85,32 @@ router.get("/api/charts/weekChart", isAuthenticated, async (req, res) => {
   });
 
   expDocument.expenses.map((exp) => {
-    const date = exp.date;
+    const date = new Date(exp.date);
     const day = date.getDay(); //0 - sunday
     let hour = roundToNearestHour(date).getHours();
 
-    if (hour<7) hour = 24 + hour;
+    if (hour < 7) hour = 24 + hour;
 
     if (hour % 2 === 0) hour--;
 
-    const hourIndex = (hour - 7) / 2 ;
+    const hourIndex = (hour - 7) / 2;
     days[day][hourIndex].value += exp.price;
   });
+
+  const dtCurrent = new Date(2022,7,4);
+  const year = dtCurrent.getFullYear();
+  const month = dtCurrent.getMonth();
+  const dtStart = new Date(year, month, 1);
+
+  const weeksPassedInMonths = differenceInWeeks(dtCurrent, dtStart) + 1;
+
+  if (weeksPassedInMonths > 1) {
+    for (let i = 0; i < 7; i++) {
+      for (let j = 0; j < 12; j++) {
+        days[i][j].value = days[i][j].value / weeksPassedInMonths;
+      }
+    }
+  }
 
   let list = [];
   for (let i = 0; i < 7; i++) {
